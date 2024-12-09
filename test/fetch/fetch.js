af25a54e6938306de7131c2328fb91bb621cc7d2
NotHansCYDIa <hanscydiastudios@gmail.com>
2024-12-09 17:30:37 +0800

    fetch.js creation

---
+++ test/fetch/fetch.js
@@ -0,0 +1,16 @@
+fetch("https://github.com/NotHansCYDIa/Lighthouse/raw/refs/heads/main/bed/sample.json")
+  .then(r => {
+    if (!r.ok) {
+      throw new Error("HTTP Error: " + r.status);
+    }
+    return r.json()
+  })
+  .then(data => {
+    console.log("STRING: " + data["string"]);
+    console.log("INT: " + data["int"]);
+    console.log("FLOAT: " + data["float"]);
+    console.log("SUCCESSFUL: " + data["success"]);
+  })
+  .catch(e => {
+    throw new Error("Unexpected Error: " + e);
+  })
