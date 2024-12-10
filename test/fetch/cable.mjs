8ef7490bf216cc358248b6ed6423fddf4b44cb0a
NotHansCYDIa <hanscydiastudios@gmail.com>
2024-12-10 16:58:34 +0800

    im so dum

--- test/fetch/cable.mjs
+++ test/fetch/cable.mjs
@@ -1,3 +1,3 @@
-import { fetchFrontEnd, frontEndData } from '../../services/fetcher.js';
+import { fetchBackEnd, backEndData } from '../../services/fetcher.js';
 fetchBackEnd(backEndData.sample)
   .then(data => console.log(data)) // test.
